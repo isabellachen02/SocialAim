aa14a135692a88001b02e5c11fd4a8a951cda0fa
Isabella Chen <chenisabella02@gmail.com>
2020-08-12 13:15:30 -0700

    Initial Commit

---
+++ tabbedApp.take1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  tabbedApp.take1
+//
+//  Created by Bella on 8/12/20.
+//  Copyright © 2020 Isabella Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
